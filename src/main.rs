435bfa5e71903dbd0a892f11059da825037ba30a
Nemya <yiduolnemya@gmail.com>
2025-02-23 14:45:22 -0500

    feat: move file reading to a function

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{env, fs, process};
+use std::{env, error::Error, fs, process};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -10,9 +10,17 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
 
-    let contents =
-        fs::read_to_string(config.file_path).expect("Should have been able to read the file");
-    println!("With text: \n{contents}")
+    if let Err(e) = run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
+    }
+}
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
+
+    println!("With text:\n{contents}");
+
+    Ok(())
 }
 struct Config {
     query: String,
@@ -23,7 +31,6 @@ impl Config {
         if args.len() < 3 {
             return Err("not enough arguments");
         }
-
         let query = args[1].clone();
         let file_path = args[2].clone();
 
